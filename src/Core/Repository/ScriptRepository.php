25b6da34b72f852377fe2cd0d7784bbea83e63d2
kentforth <yashaar@mail.ru>
2019-06-18 18:38:06 +0700

    retrieve all game scripts by game id

---
+++ src/Core/Repository/ScriptRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Core\Repository;
+
+
+use App\Core\Entity\Script;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ScriptRepository
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * ScriptRepository constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function findAllScriptsByGameId(int $gameId): array
+    {
+        return $this->entityManager->getRepository(Script::class)->findBy(['game' =>$gameId], ['step' => 'ASC']);
+    }
+
+}
